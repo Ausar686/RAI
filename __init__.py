25b0b2e5ffc1355c95ffb9011a8b7e247a89ae8e
Ausar686 <glebyushkov@mail.ru>
2023-08-02 21:04:18 +0300

    Added chat backend classes

--- __init__.py
+++ __init__.py
@@ -1,8 +1,7 @@
+from .chat import Chat, ChatMenu, Message
 from .heuristics import Poll, PollGenerator
 from .media import VideoFile, AudioFile, ImageFile, Document
-from .message import Message
 from .profile import Profile
-from .storages import Actions, Diary, MediaStorage, UserData
 from .qagpt import QAGPT
 from .rbot import RBot, __version__
-from .util import message_to_openai
+from .storages import Actions, Diary, MediaStorage, UserData
---
+++ chat/__init__.py
@@ -0,0 +1,3 @@
+from .chat import Chat
+from .chat_menu import ChatMenu
+from .message import Message
